18003f15595367ee218415fafdc6c8664805130d
Fernando Barreto da Costa <f.costa@accesscredit.com.br>
2021-08-01 13:38:26 -0400

    Geracao automatica das tabelas do banco de dados

--- src/main/java/com/fernando/ordemservico/domain/OrdemServico.java
+++ src/main/java/com/fernando/ordemservico/domain/OrdemServico.java
@@ -3,11 +3,19 @@ package com.fernando.ordemservico.domain;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class OrdemServico implements Serializable{
 
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private Date data;
 	private String convenio;
